0f97c9e20ce3691387031052ffb0e1335dea7a88
Roman Bordeichuk <bordei4ykr@gmail.com>
2025-12-30 11:07:00 +0200

    hotfix-1

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,13 @@
-# Використовуємо легкий базовий образ
 FROM node:18-alpine
 
-# Створюємо робочу директорію
 WORKDIR /usr/src/app
 
-# Копіюємо файли залежностей
 COPY package*.json ./
 
-# Встановлюємо залежності
 RUN npm install --production
 
-# Копіюємо вихідний код
 COPY . .
 
-# Відкриваємо порт, на якому працює Express
 EXPOSE 8080
 
-# Запускаємо додаток
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 app.get('/', (req, res) => {
     res.send(`
         <body style="background-color: #1c1c1c; color: white; font-family: sans-serif; text-align: center; padding-top: 50px;">
-            <h1 style="color: #00ff00;">🚀 CI/CD Pipeline Success!</h1>
+            <h1 style="color: #00ff00;">🚀 CI/CD Pipeline Success 1!</h1>
             <p style="font-size: 1.2em;">Цей додаток було автоматично задеплоєно через <b>Jenkins</b> та <b>Ansible</b>.</p>
             <div style="border: 1px solid #444; display: inline-block; padding: 20px; border-radius: 10px; background: #2a2a2a;">
                 <p><b>Worker IP:</b> 192.168.56.12</p>
